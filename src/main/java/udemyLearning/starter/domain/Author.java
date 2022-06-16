5b62ba2c47fa14afc8eeb985c21c7c7772829f50
RT <sauravselakoti@gmail.com>
2022-06-17 00:22:36 +0530

    Added functions to show author list in browser using thymeleaf

--- src/main/java/udemyLearning/starter/domain/Author.java
+++ src/main/java/udemyLearning/starter/domain/Author.java
@@ -61,7 +61,6 @@ public class Author {
                 "id=" + id +
                 ", firstname='" + firstname + '\'' +
                 ", lastName='" + lastName + '\'' +
-                ", books=" + books +
                 '}';
     }
 
